9cf8928058f98e519dfb993ff80dfa94e01a1e4c
TomJensWilliams <tomjenswilliams@gmail.com>
2024-07-03 23:56:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 console.log('Hello World!');
+
 const cartonInput = document.getElementById("cartonInput");
-const fourPersonElement = document.getElementzById("fourPersonElement");
-const fivePersonElement = document.getElementzById("fivePersonElement");
-const sixPersonElement = document.getElementzById("sixPersonElement");
+const fourPersonLow = document.getElementzById("fourPersonLow");
+const fourPersonHigh = document.getElementzById("fourPersonHigh");
+const fivePersonLow = document.getElementzById("fivePersonLow");
+const fivePersonHigh = document.getElementzById("fivePersonHigh");
+const sixPersonLow = document.getElementzById("sixPersonLow");
+const sixPersonHigh = document.getElementzById("sixPersonHigh");
 
-personInput.addEventListener("keyUp", () => {
-let cartons = cartonInput.value;
-
-
-fourPersonElement.innerHTML = "";
-fivePersonElement.innerHTML = "";
-sixPersonElement.innerHTML = "";
-}
+cartonInput.addEventListener("keyUp", () => {
+  let cartons = cartonInput.value;
+  fourPersonLow.innerHTML = "1";
+  fourPersonHigh.innerHTML = "2";
+  fivePersonLow.innerHTML = "3";
+  fivePersonHigh.innerHTML = "4";
+  sixPersonLow.innerHTML = "5";
+  sixPersonHigh.innerHTML = "6";
+});
