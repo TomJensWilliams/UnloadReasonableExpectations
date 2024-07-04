babd0c2cbe4217b9894ef4c50ec85a0fb2cc119e
TomJensWilliams <tomjenswilliams@gmail.com>
2024-07-03 23:46:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hello World!');
+const cartonInput = document.getElementById("cartonInput");
+const fourPersonElement = document.getElementzById("fourPersonElement");
+const fivePersonElement = document.getElementzById("fivePersonElement");
+const sixPersonElement = document.getElementzById("sixPersonElement");
+
+personInput.addEventListener("keyUp", () => {
+let cartons = cartonInput.value;
+
+
+fourPersonElement.innerHTML = "";
+fivePersonElement.innerHTML = "";
+sixPersonElement.innerHTML = "";
+}
