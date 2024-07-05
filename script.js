59cf47701dccbea446f42615053ac4468c7e31a3
TomJensWilliams <tomjenswilliams@gmail.com>
2024-07-04 23:25:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,64 @@ const sixPersonValue = document.getElementById("sixPersonValue");
 
 cartonButton.addEventListener("click", () => {
   console.log("Hello Again!");
-  let cartons = cartonInput.value;
-  let valuesArray = [];
-  fourPersonValue.innerHTML = "1";
-  fivePersonValue.innerHTML = "3";
-  sixPersonValue.innerHTML = "5";
+  let currentCartons = cartonInput.value;
+  let valueFunction(peopleInput, cartonInput) {
+    let rateValue, constantValue;
+    if(peopleInput === 4) {
+      if(cartonInput <= 1000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 1500) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2500) {
+        rateValue = ;
+        constantValue = ;
+      } else { // Assuming cartons cannot excede 3000
+        rateValue = ;
+        constantValue = ;
+      }
+    } else if(peopleInput === 5) {
+      if(cartonInput <= 1000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 1500) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2500) {
+        rateValue = ;
+        constantValue = ;
+      } else { // Assuming cartons cannot excede 3000
+        rateValue = ;
+        constantValue = ;
+      }
+    } else { // Assuming that the truck will be worked by 4, 5, or 6 people.
+      if(cartonInput <= 1000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 1500) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2000) {
+        rateValue = ;
+        constantValue = ;
+      } else if(cartonInput <= 2500) {
+        rateValue = ;
+        constantValue = ;
+      } else { // Assuming cartons cannot excede 3000
+        rateValue = ;
+        constantValue = ;
+      }
+    }
+    return (cartonInput - constantValue) / rateValue;
+  }
+  fourPersonValue.innerHTML = valueFunction(4, currentCartons);
+  fivePersonValue.innerHTML = valueFunction(5, currentCartons);
+  sixPersonValue.innerHTML = valueFunction(6, currentCartons);
 });
