6e23f18a7dbd9a43fb07be643df518b1c9f848f1
TomJensWilliams <tomjenswilliams@gmail.com>
2024-07-05 23:59:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,53 +25,50 @@ function valueFunction(peopleInput, cartonInput) {
       rateValue = 500 / 32;
       constantValue = -546.875;
     }
-//
-    } else if(peopleInput === 5) {
-      if(cartonInput <= 1000) {
-        rateValue = 500 / 25;
-        constantValue = -580;
-      } else if(cartonInput <= 1500) {
-        rateValue = 500 / 26;
-        constantValue = -13500 / 26;
-      } else if(cartonInput <= 2000) {
-        rateValue = 500 / 25;
-        constantValue = -600;
-      } else if(cartonInput <= 2500) {
-        rateValue = 500 / 26;
-        constantValue = -500;
-      } else { // Assuming cartons cannot excede 3000
-        rateValue = 500 / 25;
-        constantValue = -620;
-      }
-    } else { // Assuming that the truck will be worked by 4, 5, or 6 people.
-      if(cartonInput <= 1000) {
-        rateValue = 500 / 21;
-        constantValue = -12000 / 21;
-      } else if(cartonInput <= 1500) {
-        rateValue = 500 / 21;
-        constantValue = -12000 / 21;
-      } else if(cartonInput <= 2000) {
-        rateValue = 500 / 22;
-        constantValue = -10500 / 22;
-      } else if(cartonInput <= 2500) {
-        rateValue = 500 / 21;
-        constantValue = -12500 / 21;
-      } else { // Assuming cartons cannot excede 3000
-        rateValue = 500 / 22;
-        constantValue = -12500 / 21;
-      }
+  } else if(peopleInput === 5) {
+    if(cartonInput <= 1000) {
+      rateValue = 500 / 25;
+      constantValue = -580;
+    } else if(cartonInput <= 1500) {
+      rateValue = 500 / 26;
+      constantValue = -13500 / 26;
+    } else if(cartonInput <= 2000) {
+      rateValue = 500 / 25;
+      constantValue = -600;
+    } else if(cartonInput <= 2500) {
+      rateValue = 500 / 26;
+      constantValue = -500;
+    } else { // Assuming cartons cannot excede 3000
+      rateValue = 500 / 25;
+      constantValue = -620;
     }
+  } else { // Assuming that the truck will be worked by 4, 5, or 6 people.
+    if(cartonInput <= 1000) {
+      rateValue = 500 / 21;
+      constantValue = -12000 / 21;
+    } else if(cartonInput <= 1500) {
+      rateValue = 500 / 21;
+      constantValue = -12000 / 21;
+    } else if(cartonInput <= 2000) {
+      rateValue = 500 / 22;
+      constantValue = -10500 / 22;
+    } else if(cartonInput <= 2500) {
+      rateValue = 500 / 21;
+      constantValue = -12500 / 21;
+    } else { // Assuming cartons cannot excede 3000
+      rateValue = 500 / 22;
+      constantValue = -12500 / 21;
+    }
+  }
+  calculatedMinutes = (cartonInput - constantValue) / rateValue;
+  remainder = calculatedMinutes % 60;
+  quotient = (calculatedMinutes - remainder) / 60;
+  return `${quotient} hours and ${remainder} minutes`;
+}
 
 cartonButton.addEventListener("click", () => {
   console.log("Another Hello!!");
   let currentCartons = cartonInput.value;
-  
-    calculatedMinutes = (cartonInput - constantValue) / rateValue;
-    remainder calculatedMinutes % 60;
-    quotient = (calculatedMinutes - remainder) / 60;
-    return `${quotient} hours and ${remainder} minutes`;
-  }
-    
   fourPersonValue.innerHTML = valueFunction(4, currentCartons);
   fivePersonValue.innerHTML = valueFunction(5, currentCartons);
   sixPersonValue.innerHTML = valueFunction(6, currentCartons);
